98b6ca34392de5ee684236bf4e5d00be74cafa1e
Sarthak Ahuja <sarthakahuja11@users.noreply.github.com>
2021-10-29 23:58:46 +0100

    Added first lab work files, to be updated

---
+++ Lab1_b.m
@@ -0,0 +1,39 @@
+%% PARAMETERS
+%g = 9.81; %m/s^2
+%m = 5;    %kg
+%l = 0.4;  %m
+%c = 0.5;  %Nm/rad
+%I=m*(l/2)^2;
+%wn=sqrt(m*g*l/(2*I));
+%xi=c/(2*I*wn);
+
+Rm = 8.4;
+kt = 0.042;
+km = 0.042;
+Jm = 0.000004;
+Lm = 1.16;
+Mh = 0.0106;
+Rh = 0.111;
+Md = 0.053;
+Rd = 0.0248;
+
+Jeq = Jm + 0.5*Mh*Rh^2 + 0.5*Md*Rd^2;
+
+k = 1;
+ 
+num = [10 100];  % assign from right to left?
+dem = [1 100];
+
+
+
+%% TRANSFER FUNCTION
+d = tf(num,dem);
+
+
+c = k*d ;
+
+l = c*p ;
+
+
+
+
